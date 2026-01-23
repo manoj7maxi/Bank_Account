4fb6e9bc8fad9fdf1ce0cf636805928c43d1678c
manoj7maxi <manoj7maxi@gmail.com>
2026-01-23 08:54:36 +0530

    Update deposit and withdrawal amounts in BankService

--- BankService.java
+++ BankService.java
@@ -38,11 +38,11 @@ public class BankService {
         System.out.println("Initial Balance: " + acc.getBalance());
 
         // Perform operations
-        acc.deposit(700);
-        System.out.println("Balance after deposit of 500: " + acc.getBalance());
+        acc.deposit(900);
+        System.out.println("Balance after deposit of 900: " + acc.getBalance());
 
-        acc.withdraw(100);
-        System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
+        acc.withdraw(500);
+        System.out.println("Balance after withdrawal of 500: " + acc.getBalance());
 
         // Print final result
         System.out.println("Final Balance: " + acc.getBalance());
